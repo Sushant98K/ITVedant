a76bef6ebc3117bb4b307c1fc3daeeed64b92168
Sushant98K <sushant98k@gmail.com>
2024-09-25 00:35:46 +0530

    Display Details on console

---
+++ Advance Web Development/5/3/details.js
@@ -0,0 +1,5 @@
+function sub() {
+  const firstname = document.getElementById("first").value;
+  const lastname = document.getElementById("last").value;
+  console.log("Firstname: " + firstname + ", Lastname: " + lastname);
+}
