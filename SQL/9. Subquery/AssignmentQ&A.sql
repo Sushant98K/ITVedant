ac58265ef1547e4ed64352d4addeb7fca1e5b582
Sushant98K <sushant98k@gmail.com>
2024-10-11 23:50:42 +0530

    Assignments

--- SQL/9. Subquery/AssignmentQ&A.sql
+++ SQL/9. Subquery/AssignmentQ&A.sql
@@ -14,4 +14,3 @@ where id in (select distinct(manager_id) from employee where manager_id > 0);
 -- Write a query to find first_name, last_name, salary of the employee whose salary is greater than average salary of all the departments. Arrange the records in the descending order of salary.
 
 select first_name, last_name, salary from employee where salary > (select avg(salary) from employee) order by salary desc;
-
