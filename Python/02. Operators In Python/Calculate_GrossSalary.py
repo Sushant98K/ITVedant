7a8c675165e56e94fad3b34db0204ec0232d1745
Sushant98K <sushant98k@gmail.com>
2024-11-10 01:17:36 +0530

    Python Assignment

---
+++ Python/02. Operators In Python/Calculate_GrossSalary.py
@@ -0,0 +1,8 @@
+basic_salary=10000
+
+Diet_Allowance = (20/100) * basic_salary
+House_Rent_Allowance = (30/100) * basic_salary
+
+Gross_Salary = basic_salary + House_Rent_Allowance + Diet_Allowance
+
+print(Gross_Salary)
