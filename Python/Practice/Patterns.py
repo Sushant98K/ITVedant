9b273c8493eb79c275d46087c2531934c1900984
Sushant98K <sushant98k@gmail.com>
2024-11-15 19:49:02 +0530

    Python Patterns

--- Python/Practice/Patterns.py
+++ Python/Practice/Patterns.py
@@ -90,3 +90,12 @@ for i in range (1,6):
    * *
     * 
 '''
+
+for i in range(1,6):
+    for space in range(5,i-1,-1):
+        print(' ', end = ' ')
+    for j in range(1,i+1):
+        print('*', end = ' ')
+    for j in range(1,i):
+        print('*', end = ' ')
+    print()
