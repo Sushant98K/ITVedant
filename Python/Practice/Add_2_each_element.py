5e6b7761d72c351df138b9b063e8b862ce04b422
Sushant98K <sushant98k@gmail.com>
2024-11-21 22:44:25 +0530

    Python Practice

---
+++ Python/Practice/Add_2_each_element.py
@@ -0,0 +1,11 @@
+og_list = [10,21,11,25,32,33,10,35,45,68,10,35,45,12,21]
+
+#add 2/number to each element
+new_list = []
+
+for i in og_list:
+    new_list.append(i+2)
+
+print(og_list)
+
+print(new_list)
