f7cb0fe5d2b9e8783798d7015c1a08d29f5e1dbf
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-02-03 02:44:03 +0530

    Blog Project

---
+++ Django/02. Django Model/Blog_Project/BlogApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User     # Built-in User model 
+
+# Custom Post model
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # One-to-many relationship
+
+    def __str__(self):
+        return self.title
