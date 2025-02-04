561db50a8cc3aa383f4b0cbff662786238f48d7c
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-02-04 13:35:49 +0530

    use Strict Method

---
+++ Advanced Web Devlopment/mock test/useStrict.js
@@ -0,0 +1,11 @@
+function sum(num1, num2) {
+
+    "use strict"; // strictly use given parameters
+
+    num1 = 3
+    num2 = 5
+    
+    return arguments[0] + arguments[1];
+}
+
+console.log(sum(1, 2));
