cddb72c269104ad70e3c4ccf5b075e27db261251
Sushant Kate <47480733+Sushant98K@users.noreply.github.com>
2025-05-02 20:50:49 +0530

    Assignment 4

--- Angular/02. Angular Components/dashboard/src/app/main/main.component.ts
+++ Angular/02. Angular Components/dashboard/src/app/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WelcomeComponent } from "../welcome/welcome.component";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [WelcomeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
